fb187ff530fda75daf7ea6780cfc8d303237851f
MrAK Techz <akmonsterprogrammer@gmail.com>
2024-12-05 00:40:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,7 +36,9 @@ async def start(client, message):
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            group_link = await message.chat.export_invite_link()
+            user = message.from_user.mention if message.from_user else "Dear" 
+            await client.send_message(LOG_CHANNEL, script.NEW_GROUP_TXT.format(temp.B_LINK, message.chat.title, message.chat.id, message.chat.username, group_link, total, user))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
     if not await db.is_user_exist(message.from_user.id):
