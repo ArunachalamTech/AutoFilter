d5a69215935da004a387a7ccc80fb3f187572f5b
MrAK Techz <akmonsterprogrammer@gmail.com>
2024-12-05 20:35:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -81,8 +81,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True)) # True if you want no results messages in Log Channel
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@doraemon.gesw4.mongodb.net/?retryWrites=true&w=majority&appName=Doraemon")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Doraemon")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@v1.igwnl.mongodb.net/?retryWrites=true&w=majority&appName=V1")
+DATABASE_NAME = environ.get('DATABASE_NAME', "V1")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
